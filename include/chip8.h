932c76c095f9a630804995114c968d7bcc068b68
ece.candogan <sevvalcandogan@gmail.com>
2026-01-10 01:14:15 +0300

    cpu.c fix

--- include/chip8.h
+++ include/chip8.h
@@ -2,13 +2,16 @@
 #define CHIP8
 #include <stdint.h>
 #include "timer.h"
+#include "cpu.h"
 
 class chip8
 {
 private:
     uint8_t ram[4096];
     uint16_t stack[16];
+    uint8_t keypad[16];
     bool display[64 * 32];
+    cpu cpu;
     timer delay_timer;
     timer sound_timer;
 public:
